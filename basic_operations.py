d6d4c436410fb61d6654ea162bf9088048aa553d
Muhammad-Bin-Ali <muhammad_binali@yahoo.com>
2021-08-25 00:38:59 -0400

    Added function to retrieve saved items

--- basic_operations.py
+++ basic_operations.py
@@ -6,6 +6,32 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 
+def checkIfExist(id, link):
+    try:
+        load_dotenv()  # Load config from a .env file:
+        MONGODB_URI = os.environ['MONGODB_URI']
+
+        # Connect to your MongoDB cluster:
+        client = MongoClient(MONGODB_URI)
+        db = client['main'] #retrieve main database
+        users = db['Users'] #retrieve Users collections
+        saved_links = (users.find_one({'id': id}))['saved-links']
+
+        if saved_links:
+            #if the url of the last link is not equal to the link that is currently being passed in
+            for saved_link in saved_links:
+                if saved_link[3] == link: #if the user has already saved that link
+                    client.close()
+                    return True
+
+            return False #if no matching link is found, return false
+        else:
+            client.close() #close connection
+            return False #user hasn't saved any links so return false
+        #in case an error is thrown for whatever reason, the connection should be closed.
+    except: 
+        client.close()
+
 def createUser(id, email):
     try:
         load_dotenv()  # Load config from a .env file:
@@ -57,7 +83,7 @@ def addLink(id, link, title, summary):
         return False
 
 #function to remove a saved link
-def delLink(id, link, title, summary):
+def delLink(id, title, summary):
     try:
         load_dotenv() # Load config from a .env file
         MONGODB_URI = os.environ['MONGODB_URI']
@@ -71,8 +97,9 @@ def delLink(id, link, title, summary):
         if saved_links:
             #finding index of saved link
             for i in range(len(saved_links)):
-                if saved_links[i][3] == link:
+                if saved_links[i][0] == title:
                     date = saved_links[i][1]
+                    link = saved_links[i][3]
                     break
             else:
                 client.close();
@@ -113,12 +140,34 @@ def getAll(id):
             client.close() #close connection
             return False
 
-        return links
+    except:
+        client.close()
+
+def getSaved(id, title):
+    try:
+        load_dotenv() # Load config from a .env file
+        MONGODB_URI = os.environ['MONGODB_URI']
+
+        # Connect to your MongoDB cluster:
+        client = MongoClient(MONGODB_URI)
+        db = client['main'] #retrieve main database
+        users = db['Users'] #retrieve Users collections
+      
+        doc = users.find_one({'id': id})
+        links = doc['saved-links']
+
+        for link in links:
+            if link[0] == title:
+                body = link[2]
+                client.close()
+                return body
+        
+        client.close()
+        return False
+
     except:
         client.close()
 
 
 if __name__ == "__main__":
-    # addLink("104845424937636392509", "https://www.instagram.com/direct/t/340282366841710300949128190617270476779?hl=en",
-    #  "Random title", "Test Summmary 1q2341241")
-    getAll("104845424937636392509")
+    print(getAll("104845424937636392509"))
